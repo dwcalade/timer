a3642ba8ad6c42572e365db90e7ca9714b77a81d
dwcalade <dwcalade@yahoo.ca>
2022-04-12 23:02:19 +0000

    Simple Timer

--- timer1.js
+++ timer1.js
@@ -1,2 +1,20 @@
+const timer = function(timerArr) {
 
-process.stdout.write('\x07');
+  if (!timerArr.length) return;
+
+  for (let timeStr of timerArr) {
+    const timeNum = Number(timeStr);
+
+    if (isNaN(timeNum)) continue;
+    if (timeNum < 0) continue;
+
+    setTimeout(() => {
+      console.log(`Beep at ${timeNum} seconds!`);
+      process.stdout.write('\x07');
+    }, timeNum * 1000);
+  }
+
+};
+
+const timerArr = process.argv.slice(2);
+timer(timerArr);
